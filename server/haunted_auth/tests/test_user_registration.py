440485fe00f80e796cdf684adb0f56aa32a317c7
Maria Welborn <maria.welborn@gmail.com>
2022-11-02 07:04:33 +1300

    delete user if password validation fails

--- server/haunted_auth/tests/test_user_registration.py
+++ server/haunted_auth/tests/test_user_registration.py
@@ -1,8 +1,9 @@
 import uuid
+from typing import Optional
 
 import pytest
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from pytest_django.live_server_helper import LiveServer
 
 from hsutils.test_utils import get_test_url, post_test_url
@@ -13,6 +14,20 @@ from hsutils.viewmodels import (
     get_profile,
     register,
 )
+
+
+def try_register(
+    live_server: LiveServer,
+    email: str,
+    password: str,
+    username: str,
+) -> tuple[int, Optional[SuccessResponse]]:
+    return post_test_url(
+        live_server,
+        register.path,
+        RegisterRequest(email=email, password=password, username=username),
+        SuccessResponse,
+    )
 
 
 @pytest.mark.django_db
@@ -26,11 +41,8 @@ def test_not_registered(live_server: LiveServer):
 
 @pytest.mark.django_db
 def test_register_verify_happy_path(live_server: LiveServer):
-    code, response = post_test_url(
-        live_server,
-        register.path,
-        RegisterRequest(email="test@example.com", password=uuid.uuid4().hex, username="test-user"),
-        SuccessResponse,
+    code, response = try_register(
+        live_server, email="test@example.com", password=uuid.uuid4().hex, username="test-user"
     )
     assert code == HttpResponse.status_code
     assert response.success is True
@@ -40,11 +52,10 @@ def test_register_verify_happy_path(live_server: LiveServer):
     assert user.email == "test@example.com"
     assert user.get_username() == "test-user"
 
-    code, response = post_test_url(
-        live_server,
-        register.path,
-        RegisterRequest(email="test@example.com", password=uuid.uuid4().hex, username="test-user"),
-        SuccessResponse,
+    # check that a duplicate user can't be registered
+
+    code, response = try_register(
+        live_server, email="test@example.com", password=uuid.uuid4().hex, username="test-user"
     )
     assert code == 409
     assert response.success is False
@@ -56,34 +67,29 @@ def test_register_verify_happy_path(live_server: LiveServer):
 
 
 @pytest.mark.django_db
-def test_register_invalid_data(live_server: LiveServer):
+def test_register_missing_data(live_server: LiveServer):
     # TODO these error checks are stupid, a proper exception handling is needed
-    code, response = post_test_url(
-        live_server,
-        register.path,
-        RegisterRequest(email="", password=uuid.uuid4().hex, username="test-user"),
-        SuccessResponse,
-    )
+    code, response = try_register(live_server, email="", password=uuid.uuid4().hex, username="test-user")
     assert code == 500
     assert response is None
     assert get_user_model().objects.count() == 0
 
-    code, response = post_test_url(
-        live_server,
-        register.path,
-        RegisterRequest(email="test@example.com", password="", username="test-user"),
-        SuccessResponse,
-    )
+    code, response = try_register(live_server, email="test@example.com", password="", username="test-user")
     assert code == 500
     assert response is None
     assert get_user_model().objects.count() == 0
 
-    code, response = post_test_url(
-        live_server,
-        register.path,
-        RegisterRequest(email="test@example.com", password=uuid.uuid4().hex, username=""),
-        SuccessResponse,
-    )
+    code, response = try_register(live_server, email="test@example.com", password=uuid.uuid4().hex, username="")
     assert code == 500
     assert response is None
+    assert get_user_model().objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_register_invalid_password(live_server: LiveServer):
+    code, response = try_register(live_server, email="test@example.com", password="1234", username="test-user")
+    assert code == HttpResponseBadRequest.status_code
+    assert response is not None
+    assert response.success is False
+    assert response.message is not None
     assert get_user_model().objects.count() == 0
