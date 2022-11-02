1bbffc82b6fb01af567c8558d151662d025035a1
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-11-02 22:26:46 +0100

    add access logging to backend

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,4 +2,4 @@ set -eux
 poetry run ./manage.py migrate
 poetry run ./manage.py createcachetable
 poetry run ./manage.py collectstatic --no-input -c
-poetry run gunicorn -b 0.0.0.0:8000 hauntedserver.wsgi
+poetry run gunicorn -b 0.0.0.0:8000 --access-logfile - hauntedserver.wsgi
