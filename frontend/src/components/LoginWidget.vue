3109498976dcd2e8a280740b0165b47e57c04f37
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-11-05 10:12:46 +0100

    show error message if login failed

--- frontend/src/components/LoginWidget.vue
+++ frontend/src/components/LoginWidget.vue
@@ -4,21 +4,32 @@ import { getProfile, login } from "@/components/ApiService";
 import { profileStore } from "@/components/ProfileStore";
 import BsBtn from "@/components/bootstrap/BsBtn.vue";
 import FloatingSingleLineInput from "@/components/bootstrap/FloatingSingleLineInput.vue";
+import BsAlert from "@/components/bootstrap/BsAlert.vue";
 
-@Options({ components: { FloatingSingleLineInput, BsBtn } })
+@Options({ components: { BsAlert, FloatingSingleLineInput, BsBtn } })
 export default class LoginWidget extends Vue {
   public profileInfo = profileStore();
 
   async formSubmit() {
-    await login({ username: this.username, password: this.password });
+    const loginResult = await login({
+      username: this.username,
+      password: this.password,
+    });
+    if (!loginResult.success) {
+      this.loginError = loginResult.message;
+      return;
+    }
+
     this.username = "";
     this.password = "";
+    this.loginError = "";
 
     this.profileInfo.$state = await getProfile();
   }
 
   private username: string = "";
   private password: string = "";
+  private loginError: string = "";
 }
 </script>
 
@@ -46,6 +57,11 @@ export default class LoginWidget extends Vue {
           required
           label="Password"
         />
+      </div>
+    </div>
+    <div v-show="loginError" class="mb-3 row">
+      <div class="col">
+        <bs-alert variant="danger">{{ loginError }}</bs-alert>
       </div>
     </div>
     <div class="form-group mb-3">
