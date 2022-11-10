953d8ea3fb07ae99c757f30aab1e0105c83e2e66
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-11-10 11:40:52 +0100

    add test for login view

---
+++ server/haunted_auth/tests/test_login.py
@@ -0,0 +1,70 @@
+import pytest
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseBase, HttpResponseForbidden
+from pytest_django.live_server_helper import LiveServer
+
+from hsutils.test_utils import post_test_url
+from hsutils.viewmodels import LoginRequest, SuccessResponse, login
+
+
+@pytest.mark.django_db
+def test_login(live_server: LiveServer):
+    User = get_user_model()
+    assert User.objects.count() == 0
+
+    code, response = post_test_url(
+        live_server,
+        login.path,
+        LoginRequest(password="password!!!", username="username"),
+        SuccessResponse,
+    )
+    assert code == HttpResponseForbidden.status_code
+    assert response is not None
+    assert response.success is False
+    user = User.objects.create_user(
+        is_active=False,
+        username="username",
+        email="test@example.com",
+        password="password!!!",
+    )
+    code, response = post_test_url(
+        live_server,
+        login.path,
+        LoginRequest(password="password!!!", username="username"),
+        SuccessResponse,
+    )
+    assert code == HttpResponseForbidden.status_code
+    assert response is not None
+    assert response.success is False
+
+    user.is_active = True
+    user.save()
+    code, response = post_test_url(
+        live_server,
+        login.path,
+        LoginRequest(password="password!!!", username="username"),
+        SuccessResponse,
+    )
+    assert code == HttpResponseBase.status_code
+    assert response is not None
+    assert response.success is True
+
+    code, response = post_test_url(
+        live_server,
+        login.path,
+        LoginRequest(password="password", username="username"),
+        SuccessResponse,
+    )
+    assert code == HttpResponseForbidden.status_code
+    assert response is not None
+    assert response.success is False
+
+    code, response = post_test_url(
+        live_server,
+        login.path,
+        LoginRequest(password="password!!!", username="xxx"),
+        SuccessResponse,
+    )
+    assert code == HttpResponseForbidden.status_code
+    assert response is not None
+    assert response.success is False
