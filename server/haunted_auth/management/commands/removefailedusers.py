59cf1f248569a94fbb349444c4c73493ffd89c2c
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-11-23 06:48:50 +0100

    fix output of removefailedusers command

--- server/haunted_auth/management/commands/removefailedusers.py
+++ server/haunted_auth/management/commands/removefailedusers.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 
@@ -11,8 +11,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        (total_deleted, _) = User.objects.filter(
+        (total_deleted, objects_deleted) = User.objects.filter(
             last_login__isnull=True,
             date_joined__lt=timezone.now() - timedelta(seconds=settings.EMAIL_MAIL_TOKEN_LIFE),
         ).delete()
-        self.stdout.write(f"Deleted {total_deleted} failed user(s)")
+        users_deleted = objects_deleted.get(get_user_model()._meta.label, 0)
+        self.stdout.write(f"Deleted {total_deleted} objects, {users_deleted} users")
