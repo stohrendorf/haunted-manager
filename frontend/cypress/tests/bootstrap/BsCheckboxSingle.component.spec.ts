9fdebae359e96ad5eff8c86e247a94555fb27f24
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-12-01 12:58:49 +0100

    add test for bs-checkbox-single

---
+++ frontend/cypress/tests/bootstrap/BsCheckboxSingle.component.spec.ts
@@ -0,0 +1,36 @@
+import BsCheckboxSingle from "@/components/bootstrap/BsCheckboxSingle.vue";
+
+describe("<BsCheckboxSingle />", () => {
+  it("contains the message", () => {
+    const msg = "test checkbox message";
+
+    cy.mount(BsCheckboxSingle, {
+      props: {
+        modelValue: false,
+      },
+      slots: {
+        default: msg,
+      },
+    });
+
+    cy.get("div").should("contain.text", msg);
+    cy.get("input").should("have.class", "form-check-input");
+  });
+  it("propagates the selected values", () => {
+    const component = cy.mount(BsCheckboxSingle, {
+      props: {
+        modelValue: false,
+      },
+      slots: {
+        default: "label",
+      },
+    });
+
+    cy.get("input").click().should("be.checked");
+
+    component.then(async (wrapper) => {
+      await wrapper.component.$nextTick();
+      expect(wrapper.wrapper.emitted("update:modelValue")[0][0]).to.be.true;
+    });
+  });
+});
