5bd434d6729c7f2a6f73e1461881478a1b90d48e
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-12-06 13:25:09 +0100

    tweak session list layout

--- frontend/src/components/SessionList.vue
+++ frontend/src/components/SessionList.vue
@@ -77,63 +77,74 @@ export default defineComponent({
           </div>
         </div>
 
-        <div v-if="session.tags || session.private" class="row">
-          <div class="col">
-            <span v-if="session.private">
-              <span class="bi bi-eye-slash" /> Private
-            </span>
-
-            <tag-list :tags="session.tags" />
-          </div>
-        </div>
-
-        <div v-if="session.description" class="row">
-          <div>
-            {{ session.description }}
-          </div>
-        </div>
-
-        <div class="row text-secondary">
-          <div v-if="session.players && session.players.length > 0">
-            Currently playing: {{ session.players.join(", ") }}.
-          </div>
-          <div v-else>
-            No active players in this session
-            <span class="bi bi-emoji-frown" />
-          </div>
-        </div>
-
         <div class="row">
-          <div class="col col-auto">
+          <div class="col col-4">
+            <div v-if="session.tags || session.private" class="row">
+              <div class="col">
+                <span v-if="session.private">
+                  <span class="bi bi-eye-slash" /> Private
+                </span>
+
+                <tag-list :tags="session.tags" />
+              </div>
+            </div>
+
             <div class="row">
-              <div>
-                <clipboard-copyable :value="session.id">
-                  <code>{{ session.id }}</code>
-                </clipboard-copyable>
-                <span class="text-secondary"> by {{ session.owner }}</span>
+              <div class="col col-auto">
+                <div class="row">
+                  <div>
+                    <clipboard-copyable :value="session.id">
+                      <code>{{ session.id }}</code>
+                    </clipboard-copyable>
+                    <span class="text-secondary"> by {{ session.owner }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              v-if="
+                profile.$state.username === session.owner ||
+                profile.$state.is_staff
+              "
+              class="row mt-1"
+            >
+              <div class="col col-auto">
+                <bs-btn
+                  variant="danger"
+                  small
+                  @click="deleteSession(session.id)"
+                >
+                  <i class="bi bi-trash" /> Delete
+                </bs-btn>
+                <bs-btn
+                  class="ms-1"
+                  variant="primary"
+                  small
+                  @click="$router.push('/edit-session/' + session.id)"
+                >
+                  <i class="bi bi-pencil" /> Edit
+                </bs-btn>
               </div>
             </div>
           </div>
-        </div>
 
-        <div
-          v-if="
-            profile.$state.username === session.owner || profile.$state.is_staff
-          "
-          class="row mt-1"
-        >
-          <div class="col col-auto">
-            <bs-btn variant="danger" small @click="deleteSession(session.id)">
-              <i class="bi bi-trash" /> Delete
-            </bs-btn>
-            <bs-btn
-              class="ms-1"
-              variant="primary"
-              small
-              @click="$router.push('/edit-session/' + session.id)"
-            >
-              <i class="bi bi-pencil" /> Edit
-            </bs-btn>
+          <div class="col col-8">
+            <div v-if="session.description" class="row">
+              <div>
+                {{ session.description }}
+              </div>
+            </div>
+            <hr v-if="session.description" />
+            <div class="row text-secondary">
+              <div v-if="session.players && session.players.length > 0">
+                Currently playing: {{ session.players.join(", ") }}.
+              </div>
+              <div v-else>
+                No active players in this session
+                <span class="bi bi-emoji-frown" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
